248b10bd45c4dbd4f73bf4afd7b6033efbc5d7bc
Hajime Tazaki <thehajime@gmail.com>
2018-12-06 08:58:24 +0900

    lkl: tentative.. fix struct stat for x86_64 ABI
    This is required when lkl-ed musl(libc.so) uses as alipine's
    libc.so and userspace app is buit on x86_64 ABI.
    
    I don't know if there is a proper way to handle this (tm).
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- arch/lkl/bits/stat.h
+++ arch/lkl/bits/stat.h
@@ -1,23 +1,49 @@
-/* copied from kernel definition (include/uapi/asm-generic/stat.h),
+/* copied from kernel definition (arch/lkl/include/uapi/asm-generic/stat.h),
  * but with struct timespec replaced by the corresponding correctly-sized
  * userspace types. */
 
+/* copied from arch/x86/include/uapi/asm/stat.h  */
+/* XXX: how to handle other case ? */
 struct stat {
-	unsigned long	st_dev;		/* Device.  */
-	unsigned long	st_ino;		/* File serial number.  */
+	unsigned long	st_dev;
+	unsigned long	st_ino;
+	unsigned long	st_nlink;
+
+	unsigned int		st_mode;
+	unsigned int		st_uid;
+	unsigned int		st_gid;
+	unsigned int		__pad0;
+	unsigned long	st_rdev;
+	long		st_size;
+	long		st_blksize;
+	long		st_blocks;	/* Number 512-byte blocks allocated. */
+
+	struct timespec st_atim;
+	struct timespec st_mtim;
+	struct timespec st_ctim;
+	long		__unused[3];
+};
+
+/* copied from uapi/asm-generic/stat.h */
+/* This matches struct stat64 in glibc2.1. Only used for 32 bit. */
+#if __BITS_PER_LONG != 64 || defined(__ARCH_WANT_STAT64)
+struct stat64 {
+	unsigned long long st_dev;	/* Device.  */
+	unsigned long long st_ino;	/* File serial number.  */
 	unsigned int	st_mode;	/* File mode.  */
 	unsigned int	st_nlink;	/* Link count.  */
 	unsigned int	st_uid;		/* User ID of the file's owner.  */
 	unsigned int	st_gid;		/* Group ID of the file's group. */
-	unsigned long	st_rdev;	/* Device number, if device.  */
-	unsigned long	__pad1;
-	long		st_size;	/* Size of file, in bytes.  */
+	unsigned long long st_rdev;	/* Device number, if device.  */
+	unsigned long long __pad1;
+	long long	st_size;	/* Size of file, in bytes.  */
 	int		st_blksize;	/* Optimal block size for I/O.  */
 	int		__pad2;
-	long		st_blocks;	/* Number 512-byte blocks allocated. */
+	long long	st_blocks;	/* Number 512-byte blocks allocated. */
 	struct timespec st_atim;
 	struct timespec st_mtim;
 	struct timespec st_ctim;
 	unsigned int	__unused4;
 	unsigned int	__unused5;
 };
+#endif
