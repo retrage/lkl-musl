libos-nuse/musl

arch: add lkl architecture for frankenlibc build
current tested target: arm, x86_64

Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

Makefile
@@ -31,7 +31,7 @@ CFLAGS =
 CFLAGS_AUTO = -Os -pipe
 CFLAGS_C99FSE = -std=c99 -ffreestanding -nostdinc 
 
-CFLAGS_ALL = $(CFLAGS_C99FSE)
+CFLAGS_ALL = $(CFLAGS_C99FSE) $(CFLAGS_LKL)
 CFLAGS_ALL += -D_XOPEN_SOURCE=700 -I./arch/$(ARCH) -I./src/internal -I./include
 CFLAGS_ALL += $(CPPFLAGS) $(CFLAGS_AUTO) $(CFLAGS)
 CFLAGS_ALL_STATIC = $(CFLAGS_ALL)
@@ -61,6 +61,9 @@ WRAPCC_CLANG = clang
 LDSO_PATHNAME = $(syslibdir)/ld-musl-$(ARCH)$(SUBARCH).so.1
 
 -include config.mak
+
+# exclude some files for frankenlibc
+SRCS := $(filter-out $(LKL_EXCLUDE),$(sort $(wildcard $(SRCS) $(LKL_INCLUDE))))
 
 all: $(ALL_LIBS) $(ALL_TOOLS)
 

retrage/lkl-musl

Makefile
@@ -45,7 +45,7 @@ CFLAGS =
 CFLAGS_AUTO = -Os -pipe
 CFLAGS_C99FSE = -std=c99 -ffreestanding -nostdinc 
 
-CFLAGS_ALL = $(CFLAGS_C99FSE)
+CFLAGS_ALL = $(CFLAGS_C99FSE) $(CFLAGS_LKL)
 CFLAGS_ALL += -D_XOPEN_SOURCE=700 -I$(srcdir)/arch/$(ARCH) -I$(srcdir)/arch/generic -Iobj/src/internal -I$(srcdir)/src/include -I$(srcdir)/src/internal -Iobj/include -I$(srcdir)/include
 CFLAGS_ALL += $(CPPFLAGS) $(CFLAGS_AUTO) $(CFLAGS)
 
@@ -83,6 +83,8 @@ all:
 	@exit 1
 
 else
+# exclude some files for frankenlibc
+BASE_SRCS := $(filter-out $(LKL_EXCLUDE),$(sort $(wildcard $(BASE_SRCS) $(LKL_INCLUDE))))
 
 all: $(ALL_LIBS) $(ALL_TOOLS)
 