a474c8e5da21bdc969bdd704d02b3e723073799a
Hajime Tazaki <thehajime@gmail.com>
2018-12-13 10:17:05 +0900

    lkl: fix errno handling
    Fixes: e13063aa ("add hidden version of &errno accessor function")
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- src/include/errno.h
+++ src/include/errno.h
@@ -5,7 +5,13 @@
 
 hidden int *___errno_location(void);
 
+#ifdef CONFIG_LKL
+int *__errno(void);
+#define errno (*__errno())
+#else  /* !CONFIG_LKL */
+int *__errno_location(void);
 #undef errno
 #define errno (*___errno_location())
+#endif	/* CONFIG_LKL */
 
 #endif
