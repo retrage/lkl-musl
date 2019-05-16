3663c2f26a7c5e412dd92798e8f89733ad67fd90
Hajime Tazaki <thehajime@gmail.com>
2019-05-16 17:57:11 +0900

    lkl: fix calling platform function call by accident
    musl should not call platform library; if platform is Linux it's okay,
    but Linux ELF on darwin platform then it's a matter.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- src/malloc/posix_memalign.c
+++ src/malloc/posix_memalign.c
@@ -2,10 +2,6 @@
 #include <errno.h>
 #include "malloc_impl.h"
 
-#ifdef CONFIG_LKL
-#define posix_memalign lkl_unused_posix_memalign
-#endif
-
 int posix_memalign(void **res, size_t align, size_t len)
 {
 	if (align < sizeof(void *)) return EINVAL;
