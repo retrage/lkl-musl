9f2f9c75588bc8e10a404276d4935cc91328b63a
Hajime Tazaki <thehajime@gmail.com>
2019-08-20 21:51:52 +0900

    lkl: add _exit() function for lkl syscall support
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- src/exit/_Exit.c
+++ src/exit/_Exit.c
@@ -1,16 +1,8 @@
 #include <stdlib.h>
 #include "syscall.h"
 
-#ifdef CONFIG_LKL
-void _Noreturn _exit(int);
-_Noreturn void _Exit(int ec)
-{
-	_exit(ec);
-}
-#else
 _Noreturn void _Exit(int ec)
 {
 	__syscall(SYS_exit_group, ec);
 	for (;;) __syscall(SYS_exit, ec);
 }
-#endif /* CONFIG_LKL */
